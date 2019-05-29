3be84d564f31975b78ab57d01e687d859deaefb0
antipatico <antipatico@no-reply.users.github.com>
2019-05-29 11:26:17 +0200

    CSS styling fix
    Removed old whatsappLogo (unreliable)
    
    Use z-index to show on QR on top of logo
    
    Namespace the functions for security purposes

--- BypassWhatsAppWebAntiAntiFingerprint.js
+++ BypassWhatsAppWebAntiAntiFingerprint.js
@@ -11,28 +11,29 @@
 // @grant        none
 // ==/UserScript==
 
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-async function hook() {
-  while(true) {
-    if (document.querySelector("div[title='Menu']") != null) {
-      console.log("[W-UNLOCKER] My work is done here, cya!")
-      break;
-    }
-    
-    let canvas = document.querySelector("canvas");
-    if (canvas != null && canvas.style["display"] != null) {
-      canvas.style["display"] = null;
-    }
-    
-    let whatsappLogo = document.querySelector(".web div div div div div div div div span");
-    if (whatsappLogo != null) {
-      whatsappLogo.style["display"] = "none"
-    }
-    await sleep(500);
+(function() {
+  function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
   }
-}
 
-hook();
+  async function hook() {
+    while(true) {
+      if (document.querySelector("div[title='Menu']") != null) {
+        console.log("[W-UNLOCKER] My work is done here, cya!")
+        break;
+      }
+
+      let canvas = document.querySelector("canvas");
+      if (canvas != null && canvas.style["display"] != null) {
+        canvas.style["display"] = null;
+        canvas.style["z-index"] = 1337;
+        canvas.style["width"] = "1000%";
+      }
+
+      await sleep(500);
+    }
+  }
+
+  hook();
+}());
+
